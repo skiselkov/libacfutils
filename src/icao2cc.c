bcb7a577842e93db4f275ce11fdfc21ff74337d5
Saso Kiselkov <skiselkov@gmail.com>
2017-08-03 21:07:09 +0200

    Added languages to several countries missing them.

--- src/icao2cc.c
+++ src/icao2cc.c
@@ -41,18 +41,18 @@ static icao2cc_t icao2cc_table[] = {
     { .icao = "AG", .cc = "SB", .lang = "XX" },	/* Solomon Islands */
     { .icao = "AN", .cc = "NR", .lang = "XX" },	/* Nauru */
     { .icao = "AY", .cc = "PG", .lang = "XX" },	/* Papua New Guinea */
-    { .icao = "BG", .cc = "GL", .lang = "XX" },	/* Greenland */
+    { .icao = "BG", .cc = "GL", .lang = "kl" },	/* Greenland */
     { .icao = "BI", .cc = "IS", .lang = "is" },	/* Iceland */
-    { .icao = "BK", .cc = "XK", .lang = "XX" },	/* Kosovo */
+    { .icao = "BK", .cc = "XK", .lang = "sq" },	/* Kosovo */
     { .icao = "C", .cc = "CA", .lang = "en" },	/* Canada */
     { .icao = "DA", .cc = "DZ", .lang = "ar" },	/* Algeria */
-    { .icao = "DB", .cc = "BJ", .lang = "XX" },	/* Benin */
-    { .icao = "DF", .cc = "BF", .lang = "XX" },	/* Burkina Faso */
-    { .icao = "DG", .cc = "GH", .lang = "XX" },	/* Ghana */
-    { .icao = "DI", .cc = "CI", .lang = "XX" },	/* Ivory Coast */
-    { .icao = "DN", .cc = "NG", .lang = "XX" },	/* Nigeria */
+    { .icao = "DB", .cc = "BJ", .lang = "fr" },	/* Benin */
+    { .icao = "DF", .cc = "BF", .lang = "fr" },	/* Burkina Faso */
+    { .icao = "DG", .cc = "GH", .lang = "en" },	/* Ghana */
+    { .icao = "DI", .cc = "CI", .lang = "fr" },	/* Ivory Coast */
+    { .icao = "DN", .cc = "NG", .lang = "en" },	/* Nigeria */
     { .icao = "DR", .cc = "NE", .lang = "XX" },	/* Niger */
-    { .icao = "DT", .cc = "TN", .lang = "XX" },	/* Tunisia */
+    { .icao = "DT", .cc = "TN", .lang = "ar" },	/* Tunisia */
     { .icao = "DX", .cc = "TG", .lang = "XX" },	/* Togo */
     { .icao = "EB", .cc = "BE", .lang = "fr" },	/* Belgium */
     { .icao = "ED", .cc = "DE", .lang = "de" },	/* Germany */
@@ -65,7 +65,7 @@ static icao2cc_t icao2cc_table[] = {
     { .icao = "EI", .cc = "IE", .lang = "en" },	/* Ireland */
     { .icao = "EK", .cc = "DK", .lang = "da" },	/* Denmark */
     { .icao = "EL", .cc = "LU", .lang = "de" },	/* Luxembourg */
-    { .icao = "EN", .cc = "MH", .lang = "XX" },	/* Marshall Islands */
+    { .icao = "EN", .cc = "MH", .lang = "en" },	/* Marshall Islands */
     { .icao = "EN", .cc = "NO", .lang = "nn" },	/* Norway */
     { .icao = "EP", .cc = "PL", .lang = "pl" },	/* Poland */
     { .icao = "ES", .cc = "SE", .lang = "sv" },	/* Sweden */
@@ -73,28 +73,28 @@ static icao2cc_t icao2cc_table[] = {
     { .icao = "EV", .cc = "LV", .lang = "lv" },	/* Latvia */
     { .icao = "EY", .cc = "LT", .lang = "lt" },	/* Lithuania */
     { .icao = "FA", .cc = "ZA", .lang = "en" },	/* South Africa */
-    { .icao = "FB", .cc = "BW", .lang = "XX" },	/* Botswana */
-    { .icao = "FC", .cc = "CG", .lang = "XX" },	/* Republic of the Congo */
-    { .icao = "FD", .cc = "SZ", .lang = "XX" },	/* Swaziland */
-    { .icao = "FE", .cc = "CF", .lang = "XX" },	/* Central African Republic */
-    { .icao = "FG", .cc = "GQ", .lang = "XX" },	/* Equatorial Guinea */
-    { .icao = "FH", .cc = "SH", .lang = "XX" },	/* Saint Helena Ascension */
+    { .icao = "FB", .cc = "BW", .lang = "en" },	/* Botswana */
+    { .icao = "FC", .cc = "CG", .lang = "fr" },	/* Republic of the Congo */
+    { .icao = "FD", .cc = "SZ", .lang = "en" },	/* Swaziland */
+    { .icao = "FE", .cc = "CF", .lang = "fr" },	/* Central African Republic */
+    { .icao = "FG", .cc = "GQ", .lang = "es" },	/* Equatorial Guinea */
+    { .icao = "FH", .cc = "SH", .lang = "en" },	/* Saint Helena Ascension */
 						/* and Tristan da Cunha */
     { .icao = "FI", .cc = "MU", .lang = "XX" },	/* Mauritius */
     { .icao = "FJ", .cc = "IO", .lang = "en" },	/* British Indian Ocean */
 						/* Territory */
-    { .icao = "FK", .cc = "CM", .lang = "XX" },	/* Cameroon */
+    { .icao = "FK", .cc = "CM", .lang = "fr" },	/* Cameroon */
     { .icao = "FL", .cc = "ZM", .lang = "XX" },	/* Zambia */
-    { .icao = "FMC", .cc = "KM", .lang = "XX" },	/* Comoros */
-    { .icao = "FME", .cc = "RE", .lang = "XX" },	/* Réunion */
-    { .icao = "FMM", .cc = "MG", .lang = "XX" },	/* Madagascar */
-    { .icao = "FMN", .cc = "MG", .lang = "XX" },	/* Madagascar */
-    { .icao = "FMS", .cc = "MG", .lang = "XX" },	/* Madagascar */
+    { .icao = "FMC", .cc = "KM", .lang = "XX" },/* Comoros */
+    { .icao = "FME", .cc = "RE", .lang = "XX" },/* Réunion */
+    { .icao = "FMM", .cc = "MG", .lang = "fr" },/* Madagascar */
+    { .icao = "FMN", .cc = "MG", .lang = "fr" },/* Madagascar */
+    { .icao = "FMS", .cc = "MG", .lang = "fr" },/* Madagascar */
     { .icao = "FM", .cc = "YT", .lang = "XX" },	/* Mayotte */
     { .icao = "FN", .cc = "AO", .lang = "XX" },	/* Angola */
     { .icao = "FO", .cc = "GA", .lang = "XX" },	/* Gabon */
     { .icao = "FP", .cc = "ST", .lang = "XX" },	/* São Tomé and Príncipe */
-    { .icao = "FQ", .cc = "MZ", .lang = "XX" },	/* Mozambique */
+    { .icao = "FQ", .cc = "MZ", .lang = "pt" },	/* Mozambique */
     { .icao = "FS", .cc = "SC", .lang = "XX" },	/* Seychelles */
     { .icao = "FT", .cc = "TD", .lang = "XX" },	/* Chad */
     { .icao = "FV", .cc = "ZW", .lang = "en" },	/* Zimbabwe */
@@ -111,7 +111,7 @@ static icao2cc_t icao2cc_table[] = {
     { .icao = "GG", .cc = "GW", .lang = "XX" },	/* Guinea-Bissau */
     { .icao = "GL", .cc = "LR", .lang = "XX" },	/* Liberia */
     { .icao = "GM", .cc = "MA", .lang = "ar" },	/* Morocco */
-    { .icao = "GO", .cc = "SN", .lang = "XX" },	/* Senegal */
+    { .icao = "GO", .cc = "SN", .lang = "fr" },	/* Senegal */
     { .icao = "GQ", .cc = "MR", .lang = "XX" },	/* Mauritania */
     { .icao = "GS", .cc = "EH", .lang = "XX" },	/* Western Sahara */
     { .icao = "GU", .cc = "GN", .lang = "XX" },	/* Guinea */
@@ -131,34 +131,34 @@ static icao2cc_t icao2cc_table[] = {
     { .icao = "HU", .cc = "UG", .lang = "XX" },	/* Uganda */
     { .icao = "K", .cc = "US", .lang = "en" },	/* United States */
     { .icao = "LA", .cc = "AL", .lang = "sq" },	/* Albania */
-    { .icao = "LB", .cc = "BG", .lang = "XX" },	/* Bulgaria */
+    { .icao = "LB", .cc = "BG", .lang = "bg" },	/* Bulgaria */
     { .icao = "LC", .cc = "CY", .lang = "XX" },	/* Cyprus */
     { .icao = "LD", .cc = "HR", .lang = "hr" },	/* Croatia */
     { .icao = "LE", .cc = "ES", .lang = "es" },	/* Spain */
     { .icao = "LF", .cc = "FR", .lang = "fr" },	/* France */
-    { .icao = "LF", .cc = "PM", .lang = "XX" },	/* Saint Pierre and Miquelon */
+    { .icao = "LF", .cc = "PM", .lang = "fr" },	/* Saint Pierre and Miquelon */
     { .icao = "LG", .cc = "GR", .lang = "el" },	/* Greece */
     { .icao = "LH", .cc = "HU", .lang = "hu" },	/* Hungary */
     { .icao = "LI", .cc = "IT", .lang = "it" },	/* Italy */
     { .icao = "LJ", .cc = "SI", .lang = "sl" },	/* Slovenia */
     { .icao = "LK", .cc = "CZ", .lang = "cs" },	/* Czech Republic */
     { .icao = "LL", .cc = "IL", .lang = "he" },	/* Israel */
-    { .icao = "LM", .cc = "MT", .lang = "XX" },	/* Malta */
+    { .icao = "LM", .cc = "MT", .lang = "mt" },	/* Malta */
     { .icao = "LN", .cc = "MC", .lang = "fr" },	/* Monaco */
     { .icao = "LO", .cc = "AT", .lang = "de" },	/* Austria */
     { .icao = "LP", .cc = "PT", .lang = "pt" },	/* Portugal */
-    { .icao = "LQ", .cc = "BA", .lang = "XX" },	/* Bosnia and Herzegovina */
+    { .icao = "LQ", .cc = "BA", .lang = "bs" },	/* Bosnia and Herzegovina */
     { .icao = "LR", .cc = "RO", .lang = "ro" },	/* Romania */
-    { .icao = "LS", .cc = "CH", .lang = "it" },	/* Switzerland */
+    { .icao = "LS", .cc = "CH", .lang = "de" },	/* Switzerland */
     { .icao = "LT", .cc = "TR", .lang = "tr" },	/* Turkey */
-    { .icao = "LU", .cc = "MD", .lang = "XX" },	/* Moldova */
-    { .icao = "LV", .cc = "PS", .lang = "XX" },	/* Palestine */
+    { .icao = "LU", .cc = "MD", .lang = "ro" },	/* Moldova */
+    { .icao = "LV", .cc = "PS", .lang = "ar" },	/* Palestine */
     { .icao = "LW", .cc = "MK", .lang = "mk" },	/* Macedonia */
-    { .icao = "LX", .cc = "GI", .lang = "XX" },	/* Gibraltar */
-    { .icao = "LY", .cc = "ME", .lang = "XX" },	/* Montenegro */
-    { .icao = "LY", .cc = "RS", .lang = "XX" },	/* Serbia */
+    { .icao = "LX", .cc = "GI", .lang = "en" },	/* Gibraltar */
+    { .icao = "LY", .cc = "ME", .lang = "sr" },	/* Montenegro */
+    { .icao = "LY", .cc = "RS", .lang = "sr" },	/* Serbia */
     { .icao = "LZ", .cc = "SK", .lang = "sk" },	/* Slovakia */
-    { .icao = "MB", .cc = "TC", .lang = "XX" },	/* Turks and Caicos Islands */
+    { .icao = "MB", .cc = "TC", .lang = "en" },	/* Turks and Caicos Islands */
     { .icao = "MD", .cc = "DO", .lang = "es" },	/* Dominican Republic */
     { .icao = "MG", .cc = "GT", .lang = "es" },	/* Guatemala */
     { .icao = "MH", .cc = "HN", .lang = "es" },	/* Honduras */
@@ -170,31 +170,31 @@ static icao2cc_t icao2cc_table[] = {
     { .icao = "MP", .cc = "PA", .lang = "es" },	/* Panama */
     { .icao = "MR", .cc = "CR", .lang = "es" },	/* Costa Rica */
     { .icao = "MS", .cc = "SV", .lang = "es" },	/* El Salvador */
-    { .icao = "MT", .cc = "HT", .lang = "XX" },	/* Haiti */
-    { .icao = "MU", .cc = "CU", .lang = "XX" },	/* Cuba */
-    { .icao = "MW", .cc = "KY", .lang = "XX" },	/* Cayman Islands */
-    { .icao = "MY", .cc = "BS", .lang = "XX" },	/* Bahamas */
+    { .icao = "MT", .cc = "HT", .lang = "fr" },	/* Haiti */
+    { .icao = "MU", .cc = "CU", .lang = "es" },	/* Cuba */
+    { .icao = "MW", .cc = "KY", .lang = "en" },	/* Cayman Islands */
+    { .icao = "MY", .cc = "BS", .lang = "en" },	/* Bahamas */
     { .icao = "MZ", .cc = "BZ", .lang = "en" },	/* Belize */
-    { .icao = "NC", .cc = "CK", .lang = "XX" },	/* Cook Islands */
+    { .icao = "NC", .cc = "CK", .lang = "en" },	/* Cook Islands */
     { .icao = "NE", .cc = "CL", .lang = "es" },	/* Chile */
-    { .icao = "NFT", .cc = "TO", .lang = "XX" },	/* Tonga */
+    { .icao = "NFT", .cc = "TO", .lang = "XX" },/* Tonga */
     { .icao = "NF", .cc = "FJ", .lang = "XX" },	/* Fiji */
-    { .icao = "NGF", .cc = "TV", .lang = "XX" },	/* Tuvalu */
+    { .icao = "NGF", .cc = "TV", .lang = "XX" },/* Tuvalu */
     { .icao = "NG", .cc = "KI", .lang = "XX" },	/* Kiribati */
     { .icao = "NI", .cc = "NU", .lang = "XX" },	/* Niue */
     { .icao = "NL", .cc = "WF", .lang = "XX" },	/* Wallis and Futuna */
     { .icao = "NS", .cc = "AS", .lang = "en" },	/* American Samoa */
     { .icao = "NS", .cc = "WS", .lang = "XX" },	/* Samoa */
-    { .icao = "NT", .cc = "PF", .lang = "XX" },	/* French Polynesia */
+    { .icao = "NT", .cc = "PF", .lang = "fr" },	/* French Polynesia */
     { .icao = "NV", .cc = "VU", .lang = "XX" },	/* Vanuatu */
     { .icao = "NW", .cc = "NC", .lang = "XX" },	/* New Caledonia */
     { .icao = "NZ", .cc = "NZ", .lang = "en" },	/* New Zealand */
-    { .icao = "OA", .cc = "AF", .lang = "XX" },	/* Afghanistan */
+    { .icao = "OA", .cc = "AF", .lang = "ps" },	/* Afghanistan */
     { .icao = "OB", .cc = "BH", .lang = "ar" },	/* Bahrain */
     { .icao = "OE", .cc = "SA", .lang = "ar" },	/* Saudi Arabia */
     { .icao = "OI", .cc = "IR", .lang = "fa" },	/* Iran */
     { .icao = "OJ", .cc = "JO", .lang = "ar" },	/* Jordan */
-    { .icao = "OJ", .cc = "PS", .lang = "XX" },	/* Palestine */
+    { .icao = "OJ", .cc = "PS", .lang = "ar" },	/* Palestine */
     { .icao = "OK", .cc = "KW", .lang = "ar" },	/* Kuwait */
     { .icao = "OL", .cc = "LB", .lang = "ar" },	/* Lebanon */
     { .icao = "OM", .cc = "AE", .lang = "ar" },	/* United Arab Emirates */
@@ -203,21 +203,22 @@ static icao2cc_t icao2cc_table[] = {
     { .icao = "OR", .cc = "IQ", .lang = "ar" },	/* Iraq */
     { .icao = "OS", .cc = "SY", .lang = "syr" },/* Syria */
     { .icao = "OT", .cc = "QA", .lang = "ar" },	/* Qatar */
-    { .icao = "OY", .cc = "YE", .lang = "XX" },	/* Yemen */
+    { .icao = "OY", .cc = "YE", .lang = "ar" },	/* Yemen */
     { .icao = "PA", .cc = "US", .lang = "en" },	/* United States */
     { .icao = "PB", .cc = "US", .lang = "en" },	/* United States */
     { .icao = "PF", .cc = "US", .lang = "en" },	/* United States */
-    { .icao = "PG", .cc = "GU", .lang = "XX" },	/* Guam */
-    { .icao = "PG", .cc = "MP", .lang = "XX" },	/* Northern Mariana Islands */
+    { .icao = "PG", .cc = "GU", .lang = "en" },	/* Guam */
+    { .icao = "PG", .cc = "MP", .lang = "en" },	/* Northern Mariana Islands */
     { .icao = "PH", .cc = "US", .lang = "en" },	/* United States */
     { .icao = "PJ", .cc = "US", .lang = "en" },	/* United States */
-    { .icao = "PK", .cc = "MH", .lang = "XX" },	/* Marshall Islands */
+    { .icao = "PK", .cc = "MH", .lang = "en" },	/* Marshall Islands */
     { .icao = "PL", .cc = "NZ", .lang = "en" },	/* New Zealand */
     { .icao = "PL", .cc = "US", .lang = "en" },	/* United States */
     { .icao = "PM", .cc = "US", .lang = "en" },	/* United States */
     { .icao = "PO", .cc = "US", .lang = "en" },	/* United States */
     { .icao = "PP", .cc = "US", .lang = "en" },	/* United States */
-    { .icao = "PT", .cc = "FM", .lang = "XX" },	/* Federated States of Micronesia */
+    { .icao = "PT", .cc = "FM", .lang = "XX" },	/* Federated States of */
+						/* Micronesia */
     { .icao = "PT", .cc = "PW", .lang = "XX" },	/* Palau */
     { .icao = "PW", .cc = "US", .lang = "en" },	/* United States */
     { .icao = "RC", .cc = "TW", .lang = "zh" },	/* Taiwan */
@@ -230,13 +231,13 @@ static icao2cc_t icao2cc_table[] = {
     { .icao = "SC", .cc = "CL", .lang = "es" },	/* Chile */
     { .icao = "SD", .cc = "BR", .lang = "pt" },	/* Brazil */
     { .icao = "SE", .cc = "EC", .lang = "es" },	/* Ecuador */
-    { .icao = "SF", .cc = "FK", .lang = "XX" },	/* Falkland Islands */
+    { .icao = "SF", .cc = "FK", .lang = "en" },	/* Falkland Islands */
     { .icao = "SG", .cc = "PY", .lang = "es" },	/* Paraguay */
     { .icao = "SK", .cc = "CO", .lang = "es" },	/* Colombia */
     { .icao = "SL", .cc = "BO", .lang = "es" },	/* Bolivia */
     { .icao = "SM", .cc = "SR", .lang = "XX" },	/* Suriname */
     { .icao = "SN", .cc = "BR", .lang = "pt" },	/* Brazil */
-    { .icao = "SO", .cc = "GF", .lang = "XX" },	/* French Guiana */
+    { .icao = "SO", .cc = "GF", .lang = "fr" },	/* French Guiana */
     { .icao = "SP", .cc = "PE", .lang = "es" },	/* Peru */
     { .icao = "SS", .cc = "BR", .lang = "pt" },	/* Brazil */
     { .icao = "SU", .cc = "UY", .lang = "es" },	/* Uruguay */
@@ -255,7 +256,7 @@ static icao2cc_t icao2cc_table[] = {
 						/* Virgin Islands */
     { .icao = "TJ", .cc = "PR", .lang = "es" },	/* Puerto Rico */
     { .icao = "TK", .cc = "KN", .lang = "en" },	/* Saint Kitts and Nevis */
-    { .icao = "TL", .cc = "LC", .lang = "XX" },	/* Saint Lucia */
+    { .icao = "TL", .cc = "LC", .lang = "en" },	/* Saint Lucia */
     { .icao = "TN", .cc = "AW", .lang = "nl" },	/* Aruba */
     { .icao = "TN", .cc = "BQ", .lang = "nl" },	/* Caribbean Netherlands */
     { .icao = "TN", .cc = "CW", .lang = "nl" },	/* Curaçao */
@@ -277,12 +278,12 @@ static icao2cc_t icao2cc_table[] = {
     { .icao = "UI", .cc = "RU", .lang = "ru" },	/* Russia */
     { .icao = "UK", .cc = "UA", .lang = "uk" },	/* Ukraine */
     { .icao = "UL", .cc = "RU", .lang = "ru" },	/* Russia */
-    { .icao = "UM", .cc = "BY", .lang = "XX" },	/* Belarus */
+    { .icao = "UM", .cc = "BY", .lang = "ru" },	/* Belarus */
     { .icao = "UN", .cc = "RU", .lang = "ru" },	/* Russia */
     { .icao = "UO", .cc = "RU", .lang = "ru" },	/* Russia */
     { .icao = "UR", .cc = "RU", .lang = "ru" },	/* Russia */
     { .icao = "US", .cc = "RU", .lang = "ru" },	/* Russia */
-    { .icao = "UT", .cc = "TJ", .lang = "XX" },	/* Tajikistan */
+    { .icao = "UT", .cc = "TJ", .lang = "tg" },	/* Tajikistan */
     { .icao = "UT", .cc = "TM", .lang = "XX" },	/* Turkmenistan */
     { .icao = "UT", .cc = "UZ", .lang = "uz" },	/* Uzbekistan */
     { .icao = "UU", .cc = "RU", .lang = "ru" },	/* Russia */
@@ -291,14 +292,14 @@ static icao2cc_t icao2cc_table[] = {
     { .icao = "VB", .cc = "MM", .lang = "XX" },	/* Myanmar */
     { .icao = "VC", .cc = "LK", .lang = "XX" },	/* Sri Lanka */
     { .icao = "VD", .cc = "KH", .lang = "XX" },	/* Cambodia */
-    { .icao = "VE", .cc = "IN", .lang = "te" },	/* India */
+    { .icao = "VE", .cc = "IN", .lang = "hi" },	/* India */
     { .icao = "VG", .cc = "BD", .lang = "XX" },	/* Bangladesh */
     { .icao = "VH", .cc = "HK", .lang = "zh" },	/* Hong Kong */
-    { .icao = "VI", .cc = "IN", .lang = "te" },	/* India */
+    { .icao = "VI", .cc = "IN", .lang = "hi" },	/* India */
     { .icao = "VL", .cc = "LA", .lang = "XX" },	/* Laos */
     { .icao = "VM", .cc = "MO", .lang = "zh" },	/* Macau */
     { .icao = "VN", .cc = "NP", .lang = "XX" },	/* Nepal */
-    { .icao = "VO", .cc = "IN", .lang = "te" },	/* India */
+    { .icao = "VO", .cc = "IN", .lang = "hi" },	/* India */
     { .icao = "VQ", .cc = "BT", .lang = "XX" },	/* Bhutan */
     { .icao = "VR", .cc = "MV", .lang = "div" },/* Maldives */
     { .icao = "VT", .cc = "TH", .lang = "th" },	/* Thailand */
