035977c28e4af67a0b9df48ce70e9077442e250f
Saso Kiselkov <skiselkov@gmail.com>
2017-11-09 21:21:33 +0100

    Added iter_fract function.

--- src/acfutils/math.h
+++ src/acfutils/math.h
@@ -42,6 +42,37 @@ wavg(double x, double y, double w)
 	return (x + (y - x) * w);
 }
 
+/*
+ * Given two values min_val and max_val (such that min_val < max_val),
+ * returns how far between min_val and max_val a third value 'x' lies.
+ * If `clamp' is true, 'x' is clamped such that it always lies between
+ * min_val and max_val. In essence, this function computes:
+ *
+ *      ^
+ *      |
+ *    1 -------------------+
+ *      |               /  |
+ *      |             /    |
+ * f(x) ------------+      |
+ *      |         / |      |
+ *      |       /   |      |
+ *    0 ------+     |      |
+ *      |     |     |      |
+ *      +-----|-----|------|----->
+ *         min_val  x   max_val
+ */
+static inline double
+iter_fract(double x, double min_val, double max_val, bool_t clamp)
+{
+	ASSERT3F(max_val, >, min_val);
+	x = (x - min_val) / (max_val - min_val);
+	if (clamp) {
+		x = MAX(x, 0);
+		x = MIN(x, 1);
+	}
+	return (x);
+}
+
 #ifdef	__cplusplus
 }
 #endif
