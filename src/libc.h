14cabb9f04e3f23b9e452184854f2b6ffee1a920
Saso Kiselkov <skiselkov@gmail.com>
2019-03-30 09:31:38 +0100

    Don't attempt to declare own types from stdint.h as that clashes with GCC7.

--- src/libc.h
+++ src/libc.h
@@ -41,23 +41,7 @@
     #include <string.h>
 #endif
 #include <math.h>
-
-#ifndef __int8_t_defined
-    #define __int8_t_defined
-    typedef unsigned char  uint8_t;
-    typedef   signed char   int8_t;
-    typedef unsigned short uint16_t;
-    typedef   signed short  int16_t;
-    typedef unsigned int   uint32_t;
-    typedef   signed int    int32_t;
-    #ifdef _MSC_VER
-        typedef unsigned __int64 uint64_t;
-        typedef   signed __int64  int64_t;
-    #else
-        typedef unsigned long long uint64_t;
-        typedef   signed long long  int64_t;
-    #endif
-#endif
+#include <stdint.h>
 
 #ifndef NULL
     #define NULL 0
