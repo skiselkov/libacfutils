0bf2152a2b0725d73928f02df58902fa1969f409
Saso Kiselkov <skiselkov@gmail.com>
2019-11-23 01:48:58 +0100

    lacf_strlcpy would perform horribly if src was very large

--- src/acfutils/helpers.h
+++ src/acfutils/helpers.h
@@ -228,13 +228,22 @@ API_EXPORT ssize_t filesz(const char *filename);
 static inline void
 lacf_strlcpy(char *restrict dest, const char *restrict src, size_t cap)
 {
+	size_t l;
+
 	ASSERT(cap != 0);
+	/*
+	 * We MUSTN'T use strlen here, because src may be SIGNIFICANTLY
+	 * larger than dest and we don't want to measure the ENTIRE body
+	 * of src. We only care for length UP TO the destination capacity.
+	 */
+	for (l = 0; l + 1 < cap && src[l] != '\0'; l++)
+		;
 	/*
 	 * Due to a bug in GCC, we can't use strncpy, as it sometimes throws
 	 * "call to __builtin___strncpy_chk will always overflow destination
 	 * buffer", even when it's absolutely NOT the case.
 	 */
-	memcpy(dest, src, MIN(cap - 1, strlen(src) + 1));
+	memcpy(dest, src, MIN(cap - 1, l + 1));
 	/* Insure the string is ALWAYS terminated */
 	dest[cap - 1] = '\0';
 }
