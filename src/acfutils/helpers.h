6dcf586bcedcd3d67dd573b107ef71ed49ff3979
Saso Kiselkov <skiselkov@gmail.com>
2020-05-26 16:38:22 +0200

    Added integer-Hz versions of is_valid_*_freq functions.

--- src/acfutils/helpers.h
+++ src/acfutils/helpers.h
@@ -150,10 +150,22 @@ API_EXPORT const char *extract_icao_country_code(const char *icao);
 API_EXPORT bool_t is_valid_xpdr_code(int code);
 #define	is_valid_vor_freq	ACFSYM(is_valid_vor_freq)
 API_EXPORT bool_t is_valid_vor_freq(double freq_mhz);
+static inline bool_t is_valid_vor_freq_hz(uint32_t freq_hz)
+{
+	return (is_valid_vor_freq(freq_hz / 1000000.0));
+}
 #define	is_valid_loc_freq	ACFSYM(is_valid_loc_freq)
 API_EXPORT bool_t is_valid_loc_freq(double freq_mhz);
+static inline bool_t is_valid_loc_freq_hz(uint32_t freq_hz)
+{
+	return (is_valid_loc_freq(freq_hz / 1000000.0));
+}
 #define	is_valid_ndb_freq	ACFSYM(is_valid_ndb_freq)
 API_EXPORT bool_t is_valid_ndb_freq(double freq_khz);
+static inline bool_t is_valid_ndb_freq_hz(uint32_t freq_hz)
+{
+	return (is_valid_loc_freq(freq_hz / 1000.0));
+}
 #define	is_valid_tacan_freq	ACFSYM(is_valid_tacan_freq)
 API_EXPORT bool_t is_valid_tacan_freq(double freq_mhz);
 #define	is_valid_rwy_ID	ACFSYM(is_valid_rwy_ID)
