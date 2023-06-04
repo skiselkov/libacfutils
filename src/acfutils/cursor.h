8b2dd8b278c23b3846a4483b086de6696a970fba
Saso Kiselkov <skiselkov@gmail.com>
2023-06-04 21:03:07 +0200

    Documented cursor.h

--- src/acfutils/cursor.h
+++ src/acfutils/cursor.h
@@ -20,7 +20,18 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2020 Saso Kiselkov. All rights reserved.
+ * Copyright 2023 Saso Kiselkov. All rights reserved.
+ */
+/**
+ * \file
+ * Generic implementation to let you directly set the OS-provided mouse
+ * cursor. For Mac & Linux, you need a PNG of your cursor. The cursor
+ * image is assumed to be symmetrical with its hotspot in the center of
+ * the image. For Windows, the provided filename's '.png' extension is
+ * automatically replcated with '.cur' and you must provide a cursor
+ * in Windows .cur cursor format. Simply place both the PNG and CUR
+ * versions of the cursor side-by-side and the library will select the
+ * correct one depending on the host platform.
  */
 
 #ifndef	_ACF_UTILS_CURSOR_H_
@@ -32,32 +43,24 @@
 extern "C" {
 #endif
 
-/*
- * Generic implementation to let you directly set the OS-provided mouse
- * cursor. For Mac & Linux, you need a PNG of your cursor. The cursor
- * image is assumed to be symmetrical with its hotspot in the center of
- * the image. For Windows, the provided filename's '.png' extension is
- * automatically replcated with '.cur' (and you must provide a cursor
- * in Windows .cur cursor format). Simply place bot the PNG and CUR
- * versions of the cursor side-by-side and the library will select the
- * correct one depending on the host platform.
- */
 typedef struct cursor_s cursor_t;
 
-/*
+/**
  * Loads a cursor from a PNG file. The cursor hotspot is assumed to be
  * directly in the center of the image. On Windows, the PNG extension
  * is automatically replaced by '.cur', so you need to provide both a
  * PNG and CUR version of your cursor.
+ *
+ * You must free the returned cursor object using cursor_free().
  */
 API_EXPORT cursor_t *cursor_read_from_file(const char *filename_png);
 
-/*
- * Frees a cursor previously created using cursor_read_from_file.
+/**
+ * Frees a cursor previously created using cursor_read_from_file().
  */
 API_EXPORT void cursor_free(cursor_t *cursor);
 
-/*
+/**
  * Sets the cursor to be the current cursor. Be sure to ask X-Plane not
  * to draw its own cursor. This function should be called in the window
  * mouse cursor handler callback.
