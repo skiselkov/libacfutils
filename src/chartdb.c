skiselkov/libacfutils

commit 5ff7544836241ae9b31ede578203ecf1e8b348cd

Documented chartdb functionality.

src/chartdb.c
@@ -13,7 +13,7 @@
  * CDDL HEADER END
 */
 /*
- * Copyright 2022 Saso Kiselkov. All rights reserved.
+ * Copyright 2023 Saso Kiselkov. All rights reserved.
  */
 
 #include <errno.h>
@@ -655,7 +655,7 @@ chartdb_pdf_convert_file(const char *pdftoppm_path, char *old_path, int page,
 	if (png_buf == NULL)
 		goto errout;
 
-	new_path = strdup(old_path);
+	new_path = safe_strdup(old_path);
 	ext = strrchr(new_path, '.');
 	VERIFY(ext != NULL);
 	lacf_strlcpy(&ext[1], "png", strlen(&ext[1]) + 1);
@@ -1211,7 +1211,7 @@ loader_fini(void *userinfo)
 chartdb_t *
 chartdb_init(const char *cache_path, const char *pdftoppm_path,
     const char *pdfinfo_path, unsigned airac, const char *provider_name,
-    void *provider_info)
+    const chart_prov_info_login_t *provider_login)
 {
 	chartdb_t *cdb;
 	chart_prov_id_t pid;
@@ -1220,7 +1220,7 @@ chartdb_init(const char *cache_path, const char *pdftoppm_path,
 	/* pdftoppm_path can be NULL */
 	/* pdfinfo_path can be NULL */
 	ASSERT(provider_name != NULL);
-	/* provider_info can be NULL */
+	/* provider_login can be NULL */
 
 	for (pid = 0; pid < NUM_PROVIDERS; pid++) {
 		if (strcmp(provider_name, prov[pid].name) == 0)
@@ -1240,7 +1240,22 @@ chartdb_init(const char *cache_path, const char *pdftoppm_path,
 		cdb->pdfinfo_path = safe_strdup(pdfinfo_path);
 	cdb->airac = airac;
 	cdb->prov = pid;
-	cdb->prov_info = provider_info;
+	/* Deep-copy the login information */
+	if (provider_login != NULL) {
+		cdb->prov_login = safe_malloc(sizeof (*cdb->prov_login));
+		if (provider_login->username != NULL) {
+			cdb->prov_login->username = safe_strdup(
+			    provider_login->username);
+		}
+		if (provider_login->password != NULL) {
+			cdb->prov_login->password = safe_strdup(
+			    provider_login->password);
+		}
+		if (provider_login->cainfo != NULL) {
+			cdb->prov_login->cainfo = safe_strdup(
+			    provider_login->cainfo);
+		}
+	}
 	cdb->normalize_non_icao = B_TRUE;
 	/* Default to 1/32 of physical memory, but no more than 256MB */
 	cdb->load_limit = MIN(physmem() >> 5, 256 << 20);
@@ -1287,7 +1302,22 @@ chartdb_fini(chartdb_t *cdb)
 	free(cdb->path);
 	free(cdb->pdftoppm_path);
 	free(cdb->pdfinfo_path);
-	free(cdb);
+	if (cdb->prov_login != NULL) {
+		if (cdb->prov_login->username != NULL) {
+			ZERO_FREE_N(cdb->prov_login->username,
+			    strlen(cdb->prov_login->username));
+		}
+		if (cdb->prov_login->password != NULL) {
+			ZERO_FREE_N(cdb->prov_login->password,
+			    strlen(cdb->prov_login->password));
+		}
+		if (cdb->prov_login->cainfo != NULL) {
+			ZERO_FREE_N(cdb->prov_login->cainfo,
+			    strlen(cdb->prov_login->cainfo));
+		}
+		ZERO_FREE(cdb->prov_login);
+	}
+	ZERO_FREE(cdb);
 }
 
 bool_t
@@ -1609,6 +1639,16 @@ chartdb_get_chart_surface(chartdb_t *cdb, const char *icao,
 {
 	chart_t *chart;
 
+	ASSERT(cdb != NULL);
+	ASSERT(icao != NULL);
+	ASSERT(chart_name != NULL);
+	ASSERT(surf != NULL);
+	/* num_pages can be NULL */
+
+	*surf = NULL;
+	if (num_pages != NULL)
+		*num_pages = 0;
+
 	mutex_enter(&cdb->lock);
 
 	chart = chart_find(cdb, icao, chart_name);
@@ -1670,11 +1710,11 @@ get_metar_taf_common(chartdb_t *cdb, const char *icao, bool_t metar)
 	if (metar && now - arpt->metar_load_t < MAX_METAR_AGE) {
 		/* Fresh METAR still cached, return that. */
 		if (arpt->metar != NULL)
-			result = strdup(arpt->metar);
+			result = safe_strdup(arpt->metar);
 	} else if (!metar && now - arpt->taf_load_t < MAX_TAF_AGE) {
 		/* Fresh TAF still cached, return that. */
 		if (arpt->taf != NULL)
-			result = strdup(arpt->taf);
+			result = safe_strdup(arpt->taf);
 	} else {
 		if (metar) {
 			if (!list_link_active(
@@ -1687,7 +1727,7 @@ get_metar_taf_common(chartdb_t *cdb, const char *icao, bool_t metar)
 			}
 			/* If we have an old METAR, return that for now */
 			if (arpt->metar != NULL)
-				result = strdup(arpt->metar);
+				result = safe_strdup(arpt->metar);
 		} else {
 			if (!list_link_active(
 			    &cdb->loader_cmd_taf.loader_node)) {
@@ -1699,7 +1739,7 @@ get_metar_taf_common(chartdb_t *cdb, const char *icao, bool_t metar)
 			}
 			/* If we have an old TAF, return that for now */
 			if (arpt->taf != NULL)
-				result = strdup(arpt->taf);
+				result = safe_strdup(arpt->taf);
 		}
 	}
 
@@ -1737,7 +1777,7 @@ chartdb_is_arpt_known(chartdb_t *cdb, const char *icao)
 			mutex_exit(&cdb->lock); \
 			return (NULL); \
 		} \
-		field_name = strdup(arpt->field_name); \
+		field_name = safe_strdup(arpt->field_name); \
 		mutex_exit(&cdb->lock); \
 		return (field_name); \
 	} while (0)
@@ -1795,15 +1835,14 @@ download_metar_taf_common(chartdb_t *cdb, const char *icao, const char *source,
 	snprintf(query, sizeof (query), "/response/data/%s/raw_text",
 	    node_name);
 
-	if (cdb->prov_info != NULL) {
+	if (cdb->prov_login != NULL) {
 		/*
 		 * If the caller supplied a CAINFO path, we ONLY want to use
 		 * that for the METAR download. We do NOT want to send in any
 		 * user credentials, which might be meant for the main chart
 		 * data provider.
 		 */
-		chart_prov_info_login_t *prov_login = cdb->prov_info;
-		login.cainfo = prov_login->cainfo;
+		login.cainfo = cdb->prov_login->cainfo;
 	}
 
 	if (!chart_download(cdb, url, NULL, &login, error_reason, &info))
@@ -1835,7 +1874,7 @@ download_metar_taf_common(chartdb_t *cdb, const char *icao, const char *source,
 		free(path);
 		goto errout;
 	}
-	result = strdup(
+	result = safe_strdup(
 	    (char *)xpath_obj->nodesetval->nodeTab[0]->children->content);
 	xmlXPathFreeObject(xpath_obj);
 	xmlXPathFreeContext(xpath_ctx);

src/acfutils/cairo_utils.h
@@ -20,7 +20,11 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2022 Saso Kiselkov. All rights reserved.
+ * Copyright 2023 Saso Kiselkov. All rights reserved.
+ */
+/**
+ * \file
+ * This file contains various utility and helper functions for cairo graphics.
  */
 
 #ifndef	_ACF_UTILS_CAIRO_UTILS_H_
@@ -34,6 +38,10 @@
 extern "C" {
 #endif
 
+/**
+ * If `surf` is not NULL, calls cairo_surface_destroy() on it and
+ * resets the `surf` pointer to NULL afterwards.
+ */
 #define	CAIRO_SURFACE_DESTROY(surf) \
 	do {\
 		if ((surf) != NULL) { \
@@ -42,8 +50,17 @@ extern "C" {
 		} \
 	} while (0)
 
-/* For backwards compatibility with legacy apps */
+/** For backwards compatibility with legacy apps. */
 #define	mt_cairo_render_rounded_rectangle	cairo_utils_rounded_rect
+/**
+ * Appends a rounded rectangle to the current path. In essence operates
+ * exactly as cairo_rectangle(), except the corners can be rounded over.
+ * @param x X coordinate origin of the rectangle.
+ * @param y Y coordinate origin of the rectangle.
+ * @param w Width of rectangle.
+ * @param h Height of rectangle.
+ * @param radius Radius of the rounded arcs comprising the corners.
+ */
 API_EXPORT void cairo_utils_rounded_rect(cairo_t *cr, double x, double y,
     double w, double h, double radius);
 

src/chart_prov_autorouter.c
@@ -82,7 +82,7 @@ webdav_foreach_dirlist(chartdb_t *cdb, CURL *curl, const char *path,
     bool_t (*cb)(chartdb_t *cdb, CURL *curl, const char *href))
 {
 	chart_dl_info_t dl = { .cdb = NULL };
-	chart_prov_info_login_t *login = cdb->prov_info;
+	const chart_prov_info_login_t *login = cdb->prov_login;
 	xmlDoc *doc = NULL;
 	xmlXPathContext *xpath_ctx = NULL;
 	xmlXPathObject *xpath_obj = NULL;
@@ -377,9 +377,9 @@ update_index(chartdb_t *cdb)
 bool_t
 chart_autorouter_init(chartdb_t *cdb)
 {
-	chart_prov_info_login_t *login = cdb->prov_info;
+	const chart_prov_info_login_t *login = cdb->prov_login;
 
-	VERIFY(cdb->prov_info != NULL);
+	VERIFY(cdb->prov_login != NULL);
 	VERIFY(login->username != NULL);
 	VERIFY(login->password != NULL);
 
@@ -408,13 +408,13 @@ chart_autorouter_get_chart(chart_t *chart)
 	bool_t result;
 	char url[512];
 	CURL *curl = NULL;
-	chart_prov_info_login_t *login;
+	const chart_prov_info_login_t *login;
 
 	ASSERT(chart->arpt != NULL);
 	arpt = chart->arpt;
 	ASSERT(arpt->db != NULL);
 	cdb = arpt->db;
-	login = cdb->prov_info;
+	login = cdb->prov_login;
 	ASSERT(login != NULL);
 
 	filepath = chartdb_mkpath(chart);

src/chart_prov_faa.c
@@ -55,7 +55,7 @@ update_index(chartdb_t *cdb)
 	bool_t result = B_FALSE;
 
 	snprintf(url, sizeof (url), INDEX_URL, cdb->airac);
-	result = chart_download(cdb, url, index_path, cdb->prov_info,
+	result = chart_download(cdb, url, index_path, cdb->prov_login,
 	    "Error downloading chart index", NULL);
 	if (!result && file_exists(index_path, NULL)) {
 		logMsg("WARNING: failed to contact FAA servers to refresh "
@@ -330,7 +330,7 @@ chart_faa_get_chart(chart_t *chart)
 
 	filepath = chartdb_mkpath(chart);
 	snprintf(url, sizeof (url), CHART_URL, cdb->airac, chart->filename);
-	result = chart_download(cdb, url, filepath, cdb->prov_info,
+	result = chart_download(cdb, url, filepath, cdb->prov_login,
 	    "Error downloading chart", NULL);
 	if (!result && file_exists(filepath, NULL)) {
 		logMsg("WARNING: failed to contact FAA servers to refresh "

src/chart_prov_navigraph.c
@@ -244,8 +244,8 @@ start_auth(chartdb_t *cdb, navigraph_t *nav)
 	chart_dl_info_t dl_info;
 
 	ASSERT(cdb != NULL);
-	ASSERT(cdb->prov_info != NULL);
-	login = cdb->prov_info;
+	ASSERT(cdb->prov_login != NULL);
+	login = cdb->prov_login;
 	ASSERT(nav != NULL);
 
 	chart_dl_info_init(&dl_info, cdb, DEV_AUTH_ENDPT);
@@ -393,8 +393,8 @@ get_tokens(chartdb_t *cdb, navigraph_t *nav)
 
 	ASSERT(cdb != NULL);
 	ASSERT(nav != NULL);
-	ASSERT(cdb->prov_info != NULL);
-	login = cdb->prov_info;
+	ASSERT(cdb->prov_login != NULL);
+	login = cdb->prov_login;
 
 	chart_dl_info_init(&dl_info, cdb, TOKEN_ENDPT);
 
@@ -745,8 +745,8 @@ chart_navigraph_init(chartdb_t *cdb)
 
 	ASSERT(cdb != NULL);
 
-	VERIFY(cdb->prov_info != NULL);
-	login = cdb->prov_info;
+	VERIFY(cdb->prov_login != NULL);
+	login = cdb->prov_login;
 	VERIFY(login->username != NULL);
 	VERIFY(login->password != NULL);
 	VERIFY(login->cainfo != NULL);

src/chartdb_impl.h
@@ -118,9 +118,9 @@ struct chartdb_s {
 	int		(*chart_sort_func)(const void *, const void *, void *);
 
 	/* private to chart provider */
-	void		*prov_info;
-	void		*prov_priv;
-	bool_t		init_complete;
+	chart_prov_info_login_t	*prov_login;
+	void			*prov_priv;
+	bool_t			init_complete;
 
 	/* protected by `lock' */
 	list_t		loader_queue;

commit 412ca07db9672aef52dcdf8da44b8be5a2eae583

Commit 5ff7544 introduced regression due to use of uninitialized memory.

src/chartdb.c
@@ -1242,7 +1242,7 @@ chartdb_init(const char *cache_path, const char *pdftoppm_path,
 	cdb->prov = pid;
 	/* Deep-copy the login information */
 	if (provider_login != NULL) {
-		cdb->prov_login = safe_malloc(sizeof (*cdb->prov_login));
+		cdb->prov_login = safe_calloc(1, sizeof (*cdb->prov_login));
 		if (provider_login->username != NULL) {
 			cdb->prov_login->username = safe_strdup(
 			    provider_login->username);