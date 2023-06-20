skiselkov/libacfutils

commit 8447c4c5881f4ad236499fa46f348217c8e0526a

Documented hexcode.h

src/hexcode.c
@@ -13,7 +13,7 @@
  * CDDL HEADER END
 */
 /*
- * Copyright 2018 Saso Kiselkov. All rights reserved.
+ * Copyright 2023 Saso Kiselkov. All rights reserved.
  */
 
 #include <ctype.h>
@@ -22,14 +22,29 @@
 
 #include <acfutils/hexcode.h>
 
+/**
+ * Encodes binary data into a string of hex digits.
+ * @param in_raw Input binary data buffer to be encoded.
+ * @param len Input binary buffer length in bytes.
+ * @param out_enc Output buffer to hold the string of encoded hex characters.
+ *	The output is guaranteed to always be zero-terminated (provided the
+ *	buffer can hold at least 1 byte).
+ * @param out_cap Output buffer capacity. To properly encode all input into
+ *	the output, the output buffer must be able to hold at least twice
+ *	`len` bytes plus 1 (for the terminating NUL byte).
+ */
 API_EXPORT void
 hex_enc(const void *in_raw, size_t len, void *out_enc, size_t out_cap)
 {
 	const uint8_t *in_8b = in_raw;
 	char *out = out_enc;
 
+	ASSERT(in_raw != NULL || len == 0);
+	ASSERT(out_enc != NULL || out_cap == 0);
+
 	/* guarantee zero-termination of output */
-	*out = 0;
+	if (out_cap != 0)
+		*out = 0;
 	for (size_t i = 0, j = 0; i < len && j + 3 <= out_cap; i++, j += 2)
 		sprintf(&out[j], "%02x", (unsigned)in_8b[i]);
 }
@@ -46,6 +61,24 @@ xdigit2int(char c)
 	return (-1);
 }
 
+/**
+ * Decodes a string of hex characters into a binary output buffer.
+ * @param in_enc Input string of hex characters. This must NOT contain
+ *	any whitespace. It must be strictly ONLY the hex characters.
+ *	The input doesn't need to be NUL-terminated (length is provided
+ *	explicitly in the `len` parameter).
+ * @param len Number of characters in the `in_enc` buffer (excluding any
+ *	terminating NUL bytes). This must be a multiple of 2.
+ * @param out_raw Output binary buffer, which will be filled with the
+ *	decoded data.
+ * @param out_cap Output buffer length in bytes. This must be at least
+ *	half of `len`.
+ * @return `B_TRUE` if decoding succeeded, `B_FALSE` otherwise. Decoding
+ *	failure can occur if the input doesn't contain an even number of
+ *	characters, the output buffer is too small to contain the entire
+ *	decoded input, or if an invalid (non-hex) character is encountered
+ *	in the input buffer.
+ */
 API_EXPORT bool_t
 hex_dec(const void *in_enc, size_t len, void *out_raw, size_t out_cap)
 {

src/acfutils/hexcode.h
@@ -13,8 +13,9 @@
  * CDDL HEADER END
 */
 /*
- * Copyright 2018 Saso Kiselkov. All rights reserved.
+ * Copyright 2023 Saso Kiselkov. All rights reserved.
  */
+/** \file */
 
 #ifndef	_ACF_UTILS_HEXCODE_H_
 #define	_ACF_UTILS_HEXCODE_H_
@@ -27,11 +28,8 @@
 extern "C" {
 #endif
 
-#define	hex_enc	ACFSYM(hex_enc)
 API_EXPORT void hex_enc(const void *in_raw, size_t len, void *out_enc,
     size_t out_cap);
-
-#define	hex_dec	ACFSYM(hex_dec)
 API_EXPORT bool_t hex_dec(const void *in_enc, size_t len, void *out_raw,
     size_t out_cap);
 

commit 5d3cff7e8aae707a102cb8f369858795da7c7e84

Commit 8447c4c5 broke build due to missing include for ASSERT.

src/hexcode.c
@@ -20,7 +20,8 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#include <acfutils/hexcode.h>
+#include "acfutils/assert.h"
+#include "acfutils/hexcode.h"
 
 /**
  * Encodes binary data into a string of hex digits.