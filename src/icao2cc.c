skiselkov/libacfutils

commit 9c534dc601d3bc0c88394ce253b2a2c40338b1be

Documented icao2cc subsystem.

src/icao2cc.c
@@ -13,7 +13,7 @@
  * CDDL HEADER END
 */
 /*
- * Copyright 2017 Saso Kiselkov. All rights reserved.
+ * Copyright 2023 Saso Kiselkov. All rights reserved.
  */
 
 #include <stdarg.h>
@@ -421,13 +421,19 @@ static icao2cc_t icao2cc_table[] = {
     { .icao = NULL, .cc = NULL, .lang = NULL }	/* Last entry */
 };
 
-/*
+/**
  * Converts an ICAO code to a country code. This performs a simple prefix
  * match using the icao2cc_table.
+ * @return The 2-letter ISO 3166 country code using upper case. If the
+ *	country code cannot be determined, or the passed argument isn't
+ *	a valid ICAO code, returns NULL instead.
  */
 const char *
 icao2cc(const char *icao)
 {
+	ASSERT(icao != NULL);
+	if (!is_valid_icao_code(icao))
+		return (NULL);
 	/*
 	 * Doing a linear search is not particularly elegant, but the size
 	 * of the ICAO table is fixed and small, so it probably doesn't
@@ -442,13 +448,14 @@ icao2cc(const char *icao)
 	return (NULL);
 }
 
-/*
+/**
  * Grabs an ICAO airport code and tries to map it to language code of
  * the principal language spoken at that airport. This shouldn't be relied
  * upon to be very accurate, since in reality the airport-to-language
  * mapping is anything but clear cut.
- * Returns a two- or three-letter language code (if no two-letter one exists),
- * or "XX" if no suitable mapping was found.
+ *
+ * @return A two- or three-letter language code (if no two-letter one
+ *	exists) using lower case, or "XX" if no suitable mapping was found.
  */
 const char *
 icao2lang(const char *icao)

src/acfutils/icao2cc.h
@@ -13,7 +13,13 @@
  * CDDL HEADER END
 */
 /*
- * Copyright 2017 Saso Kiselkov. All rights reserved.
+ * Copyright 2023 Saso Kiselkov. All rights reserved.
+ */
+/**
+ * \file
+ * Translation functionality to convert ICAO airport codes into
+ * country codes and language codes. This is useful for building
+ * flight information systems.
  */
 
 #ifndef	_ICAO2CC_H_
@@ -25,9 +31,7 @@
 extern "C" {
 #endif
 
-#define	icao2cc		ACFSYM(icao2cc)
 API_EXPORT const char *icao2cc(const char *icao);
-#define	icao2lang	ACFSYM(icao2lang)
 API_EXPORT const char *icao2lang(const char *icao);
 
 #ifdef	__cplusplus

commit 85a3b50f68c8f12fcff774f8219fa427cd2ff478

Commit 9c534dc broke build.

src/icao2cc.c
@@ -20,7 +20,9 @@
 #include <stdio.h>
 #include <string.h>
 
-#include <acfutils/icao2cc.h>
+#include "acfutils/assert.h"
+#include "acfutils/icao2cc.h"
+#include "acfutils/helpers.h"
 
 typedef struct {
 	const char *icao;