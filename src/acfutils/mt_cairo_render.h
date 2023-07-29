e3f23dd5b83fb3a54ed7d3c5b4cf8e1bfddaba16
Saso Kiselkov <skiselkov@gmail.com>
2023-07-29 17:33:25 +0200

    Documented mt_cairo_render.h and cleaned up leftover legacy code.

--- src/acfutils/mt_cairo_render.h
+++ src/acfutils/mt_cairo_render.h
@@ -22,6 +22,14 @@
 /*
  * Copyright 2023 Saso Kiselkov. All rights reserved.
  */
+/**
+ * \file
+ * `mt_cairo_render` is a multi-threaded [Cairo](https://www.cairographics.org/)
+ * rendering surface with built-in buffering and OpenGL compositing.
+ * You only need to provide a callback that renders into the surface
+ * using a passed `cairo_t` and then call mt_cairo_render_draw() at
+ * regular intervals to display the rendered result.
+ */
 
 #ifndef	_ACF_UTILS_MT_CAIRO_RENDER_H_
 #define	_ACF_UTILS_MT_CAIRO_RENDER_H_
@@ -43,18 +51,84 @@ extern "C" {
 
 API_EXPORT void mt_cairo_render_glob_init(bool_t want_coherent_mem);
 
+/**
+ * An optional initialization callback which can be passed to
+ * mt_cairo_render_init() in the init_cb argument. This callback can be
+ * used to allocate custom resources for each surface, or to configure
+ * the default state of the renderer. This will be called for each
+ * surface that is being deinitialized and may be called multiple times
+ * during surface reinit.
+ * @param cr The `cairo_t` for which this callback is being invoked.
+ * @param userinfo The argument passed to mt_cairo_render_init() in the
+ *	`userinfo` argument.
+ * @return `B_TRUE` if you want initialization to proceed, or `B_FALSE`
+ *	if you need it to abort. Please note that returning `B_FALSE`
+ *	is only allowed during mt_cairo_render_init(), as that's the
+ *	only time the renderer can abort cleanly. You must NOT return
+ *	`B_FALSE` on subsequent surface reinit operations (this only
+ *	happens if mt_cairo_render_set_monochrome() is called).
+ */
 typedef bool_t (*mt_cairo_init_cb_t)(cairo_t *cr, void *userinfo);
+/**
+ * An optional finalization callback which can be passed to
+ * mt_cairo_render_init() in the fini_cb argument. This callback can be
+ * used to free resources allocated during initialization. This will be
+ * called for each surface that is being deinitialized and may be called
+ * multiple times during surface reinit.
+ * @param cr The `cairo_t` for which this callback is being invoked.
+ * @param userinfo The argument passed to mt_cairo_render_init() in the
+ *	`userinfo` argument.
+ */
 typedef void (*mt_cairo_fini_cb_t)(cairo_t *cr, void *userinfo);
+/**
+ * Mandatory rendering callback, which must be provided to
+ * mt_cairo_render_init() in the `render_cb` argument. This will be called
+ * every time a new frame is to be rendered.
+ * @param cr The cairo_t into which you should perform rendering.
+ *	The renderer is not reinitialized between frames and no blanking
+ *	of the rendered image is performed automatically, so if you want
+ *	redraw the entire contents, perform your own screen clearing first.
+ * @param w The width of the rendering surface in pixels.
+ * @param h The height of the rendering surface in pixels.
+ * @param userinfo The argument passed to mt_cairo_render_init() in the
+ *	`userinfo` argument.
+ */
 typedef void (*mt_cairo_render_cb_t)(cairo_t *cr, unsigned w, unsigned h,
     void *userinfo);
 typedef struct mt_cairo_render_s mt_cairo_render_t;
 typedef struct mt_cairo_uploader_s mt_cairo_uploader_t;
 
-#define	mt_cairo_render_init(__w, __h, __fps, __init_cb, __render_cb, \
-    __fini_cb, __userinfo) \
+/**
+ * Creates a new mt_cairo_render_t surface.
+ * @param w Width of the rendered surface (in pixels).
+ * @param h Height of the rendered surface (in pixels).
+ * @param fps Framerate at which the surface should be rendered.
+ *	This can be changed at any time later. Pass a zero fps value
+ *	to make the renderer only run on-request (see mt_cairo_render_once()).
+ * @param init_cb An optional initialization callback of type
+ *	mt_cairo_init_cb_t. This can be used to initialize private resources
+ *	needed during rendering. This gets called once for every cairo_t
+ *	instance (two instances for every mt_cairo_render_t, due to
+ *	double-buffering). Please note that this can be called even after
+ *	calling mt_cairo_render_init, since mt_cairo_render_set_monochrome()
+ *	will re-allocate the cairo instances. If you don't want to receive
+ *	this callback, pass NULL here.
+ * @param render_cb A mandatory rendering callback of type
+ *	mt_cairo_render_cb_t. This is called for each rendered frame.
+ * @param fini_cb An optional finalization callback of type
+ *	mt_cairo_fini_cb_t, which can be used to free resources allocated
+ *	during init_cb. Similarly to `init_cb` above, this will be called
+ *	for each surface that is being deinitialized and may be called
+ *	multiple times during surface reinit. If you don't want to receive
+ *	this callback, pass NULL here.
+ * @param userinfo An optional user info pointer for the `init_cb`,
+ *	`render_cb` and `fini_cb` callbacks.
+ * @return The initialized and started mt_cairo_render_t instance.
+ *	Returns NULL if initialization failed.
+ */
+#define	mt_cairo_render_init(w, h, fps, init_cb, render_cb, fini_cb, userinfo) \
 	mt_cairo_render_init_impl(log_basename(__FILE__), __LINE__, \
-	    (__w), (__h), (__fps), (__init_cb), (__render_cb), \
-	    (__fini_cb), (__userinfo))
+	    (w), (h), (fps), (init_cb), (render_cb), (fini_cb), (userinfo))
 API_EXPORT mt_cairo_render_t *mt_cairo_render_init_impl(const char *filename,
     int line, unsigned w, unsigned h, double fps, mt_cairo_init_cb_t init_cb,
     mt_cairo_render_cb_t render_cb, mt_cairo_fini_cb_t fini_cb, void *userinfo);
@@ -63,6 +137,7 @@ API_EXPORT void mt_cairo_render_fini(mt_cairo_render_t *mtcr);
 API_EXPORT void mt_cairo_render_set_fps(mt_cairo_render_t *mtcr, double fps);
 API_EXPORT double mt_cairo_render_get_fps(mt_cairo_render_t *mtcr);
 API_EXPORT void mt_cairo_render_enable_fg_mode(mt_cairo_render_t *mtcr);
+API_EXPORT bool_t mt_cairo_render_get_fg_mode(const mt_cairo_render_t *mtcr);
 API_EXPORT void mt_cairo_render_set_texture_filter(mt_cairo_render_t *mtcr,
     unsigned gl_filter_enum);
 API_EXPORT void mt_cairo_render_set_shader(mt_cairo_render_t *mtcr,
@@ -92,20 +167,15 @@ API_EXPORT mt_cairo_uploader_t *mt_cairo_render_get_uploader(
 API_EXPORT GLuint mt_cairo_render_get_tex(mt_cairo_render_t *mtcr);
 API_EXPORT unsigned mt_cairo_render_get_width(mt_cairo_render_t *mtcr);
 API_EXPORT unsigned mt_cairo_render_get_height(mt_cairo_render_t *mtcr);
+
+#ifdef	LACF_MTCR_DEBUG
 API_EXPORT void mt_cairo_render_set_ctx_checking_enabled(
     mt_cairo_render_t *mtcr, bool_t flag);
-
-API_EXPORT void mt_cairo_render_set_debug(mt_cairo_render_t *mtcr, bool_t flag);
-API_EXPORT bool_t mt_cairo_render_get_debug(const mt_cairo_render_t *mtcr);
+#endif	/* defined(LACF_MTCR_DEBUG) */
 
 /*
  * Must ONLY be called from the rendering callback.
  */
-typedef struct {
-	unsigned	x, y;
-	unsigned	w, h;
-} mtcr_rect_t;
-
 API_EXPORT void mt_cairo_render_rounded_rectangle(cairo_t *cr, double x,
     double y, double w, double h, double radius);
 
