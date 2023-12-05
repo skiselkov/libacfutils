skiselkov/libacfutils

commit 7fd6ee9d2bc05cd3e0414e043a0018cdd42ea745

Automatically enable dependencies for building & linking when they were built specifically by the user.

qmake/qmake.pro
@@ -46,6 +46,19 @@ QMAKE_CFLAGS += -Wunused-result
 	QMAKE_CFLAGS += -Wno-stringop-overflow -Wno-missing-field-initializers
 }
 
+win32 {
+	PLAT_LONG = win-64
+	GLEWMX_LIB = ../glew/glew-1.13.0-win-64/install/lib/libglew32mx.a
+}
+linux-g++-64 {
+	PLAT_LONG = linux-64
+	GLEWMX_LIB = ../glew/glew-1.13.0-linux-64/install/lib64/libGLEWmx.a
+}
+macx {
+	PLAT_LONG = mac-64
+	GLEWMX_LIB = ../glew/glew-1.13.0-mac-64/install/lib/libGLEWmx.a
+}
+
 # _GNU_SOURCE needed on Linux for getline()
 # DEBUG - used by our ASSERT macro
 # _FILE_OFFSET_BITS=64 to get 64-bit ftell and fseek on 32-bit platforms.
@@ -274,6 +287,19 @@ SOURCES += \
     ../ucpp/macro.c \
     ../ucpp/eval.c
 
+exists("../libpng/libpng-$$PLAT_LONG/lib/libpng16.a") {
+	HEADERS += ../src/acfutils/png.h
+	SOURCES += ../src/png.c
+}
+exists("../cairo/cairo-$$PLAT_LONG/lib/libcairo.a") {
+	HEADERS += ../src/acfutils/cairo_utils.h
+	SOURCES += ../src/cairo_utils.c
+}
+exists("../cairo/cairo-$$PLAT_LONG/lib/libcairo.a") : exists("$$GLEWMX_LIB") {
+	HEADERS += ../src/acfutils/mt_cairo_render.h
+	SOURCES += ../src/mt_cairo_render.c
+}
+
 # Optional lib components when building a non-minimal library
 contains(minimal, 1) {
 	HEADERS += \
@@ -286,11 +312,8 @@ contains(minimal, 1) {
 	    ../src/acfutils/glew_os.h \
 	    ../src/acfutils/glutils.h \
 	    ../src/acfutils/lacf_gl_pic.h \
-	    ../src/acfutils/cairo_utils.h \
-	    ../src/acfutils/mt_cairo_render.h \
 	    ../src/acfutils/odb.h \
 	    ../src/acfutils/paste.h \
-	    ../src/acfutils/png.h \
 	    ../src/acfutils/riff.h \
 	    ../src/acfutils/shader.h \
 	    ../src/acfutils/wav.h \
@@ -310,8 +333,6 @@ contains(minimal, 1) {
 	    ../src/glutils.c \
 	    ../src/lacf_gl_pic.c \
 	    ../src/minimp3.c \
-	    ../src/cairo_utils.c \
-	    ../src/mt_cairo_render.c \
 	    ../src/odb.c \
 	    ../src/paste.c \
 	    ../src/png.c \

pkg-config-deps
@@ -94,6 +94,8 @@ while [[ $# -gt 0 ]]; do
 	shift
 done
 
+# Careful: the order of libraries mentioned here matters, they must be
+# dependent-provider order, same as on the linker command line.
 if ! [ -f .minimal-deps ]; then
 	if [[ "$TESSERACT" -ne 0 ]] &&
 	    [ -f "ocr/tesseract-$ARCH/lib/pkgconfig/tesseract.pc" ]; then
@@ -110,15 +112,15 @@ if [[ "$SHAPELIB" -ne 0 ]] && \
     [ -f "shapelib/libshape-${ARCH}/lib/pkgconfig/shapelib.pc" ]; then
 	PACKAGES="$PACKAGES shapelib proj"
 fi
+if [[ "$CAIRO" -ne 0 ]] && \
+    [ -f "cairo/cairo-${ARCH}/lib/pkgconfig/cairo.pc" ]; then
+	PACKAGES="$PACKAGES cairo pixman-1"
+fi
 if [ -f "libpng/libpng-${ARCH}/lib/pkgconfig/libpng16.pc" ]; then
 	PACKAGES="$PACKAGES libpng16"
 fi
 if [ -f "freetype/freetype-${ARCH}/lib/pkgconfig/freetype2.pc" ]; then
 	PACKAGES="$PACKAGES freetype2"
-fi
-if [[ "$CAIRO" -ne 0 ]] && \
-    [ -f "cairo/cairo-${ARCH}/lib/pkgconfig/cairo.pc" ]; then
-	PACKAGES="$PACKAGES cairo pixman-1"
 fi
 
 PACKAGES="$PACKAGES libcurl openssl libxml-2.0 libpcre2-8 zlib"

commit 1b1300012fb294a42d33a85b72c93944ecf462bb

Regression in 7fd6ee9d broke building of widget.c

qmake/qmake.pro
@@ -296,7 +296,7 @@ exists("../cairo/cairo-$$PLAT_LONG/lib/libcairo.a") : exists("$$GLEWMX_LIB") {
 	    ../src/acfutils/mt_cairo_render.h \
 	    ../src/acfutils/widget.h
 	SOURCES += \
-	    ../src/mt_cairo_render.c
+	    ../src/mt_cairo_render.c \
 	    ../src/widget.c
 }
 