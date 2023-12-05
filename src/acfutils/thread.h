skiselkov/libacfutils

commit 80649cab6bfd60940545f90ba8b6dbb6b1bac446

Old GCC doesn't properly support C11's _Generic, so don't use C11 generics on old GCC versions.

src/acfutils/thread.h
@@ -32,7 +32,8 @@
 #include <windows.h>
 #endif	/* !APL && !LIN */
 
-#if	__STDC_VERSION__ >= 201112L && !defined(__STDC_NO_ATOMICS__)
+#if	__STDC_VERSION__ >= 201112L && !defined(__STDC_NO_ATOMICS__) && \
+    !(defined(__GNUC__) || __GNUC__ >= 7)
 #include <stdatomic.h>
 #elif	APL
 #include <libkern/OSAtomic.h>

src/acfutils/sysmacros.h
@@ -166,7 +166,7 @@ extern "C" {
  * TYPE_ASSERT is a compile-time assertion, but which checks that
  * the type of `x' is `type'.
  */
-#if	__STDC_VERSION__ >= 201112L
+#if	__STDC_VERSION__ >= 201112L && (!defined(__GNUC__) || __GNUC__ > 7)
 #define	CTASSERT(x) _Static_assert((x), #x)
 #define	TYPE_ASSERT(x, type) \
 	CTASSERT(_Generic((x), type: 1, default: 0) != 0)

commit 5d1cec1a60c7e27b3174d17bf046716038534882

Regression in 80649cab6 in thread.h: GCC 7 detection macro screwed up

src/acfutils/thread.h
@@ -33,7 +33,7 @@
 #endif	/* !APL && !LIN */
 
 #if	__STDC_VERSION__ >= 201112L && !defined(__STDC_NO_ATOMICS__) && \
-    !(defined(__GNUC__) || __GNUC__ >= 7)
+    (!defined(__GNUC__) || __GNUC__ >= 7)
 #include <stdatomic.h>
 #elif	APL
 #include <libkern/OSAtomic.h>